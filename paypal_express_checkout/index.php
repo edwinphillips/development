a6cfad8de9a63dddf2e14d9e5d95dfeb9bc5de38
Edwin Phillips <hello@edwinphillips.net>
2013-08-05 13:06:05 +0100

    Add PayPal express checkout

---
+++ paypal_express_checkout/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once("config.php");
+?>
+<html>
+<head></head>
+<body>
+
+    <form method="post" action="process.php">
+        <input type="hidden" name="itemname" value="Product One" />
+        <input type="hidden" name="itemnumber" value="1" />
+        <input type="hidden" name="itemprice" value="10" />
+        <input type="hidden" name="itemQty" value="1" />
+        <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif" name="submitbutt" value="PayPal Express" />
+    </form>
+
+</body>
+</html>
