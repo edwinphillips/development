94484cad43eed87fe91140dbd034f3bbdf48f75d
Edwin Phillips <hello@edwinphillips.net>
2013-08-05 13:13:04 +0100

    Paypal express: Remove live config from repo

--- paypal_express_checkout/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$PayPalMode         = 'sandbox'; // sandbox or live
-$PayPalApiUsername  = 'uk_sandbox_api1.catalyst-eu.net'; //PayPal API Username
-$PayPalApiPassword  = '1375369810'; //Paypal API password
-$PayPalApiSignature = 'AFcWxV21C7fd0v3bYYYRCpSSRl31AshyaSnNZpbu8T6NSF3pbUj5Lg4R'; //Paypal API Signature
-$PayPalCurrencyCode = 'GBP'; //Paypal Currency Code
-$PayPalReturnURL    = 'http://development.gonzo.int.catalyst-eu.net/Sandbox%20testing/'; //Point to process.php page
-$PayPalCancelURL    = 'http://development.gonzo.int.catalyst-eu.net/Sandbox%20testing/'; //Cancel URL if user clicks cancel
